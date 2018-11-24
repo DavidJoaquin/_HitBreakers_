fb1e70483e17180c0cef2d6a24ae837335ec7d3c
DavidJoaquin <David.joaquinc26@gmail.com>
2018-11-24 10:51:58 +0100

    Sistema de disparo basico
    -Todo está en la carpeta David/SitemaDisparo.
    -La escena de prueba es SistemaDisparoTest

---
+++ _HitBreakers_/Assets/David/Sistema Disparos/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+    //FLOAT - La velocidad de la bala
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //Movemos la bala hacia delante con la velocidad speed multiplicada por Time.deltaTime
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+		
+	}
+}
